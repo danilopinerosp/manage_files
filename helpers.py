d1f46ce6d9e702062ea7e69bb738446a3264b1ad
Danilo Piñeros <dspinerosp@gmail.com>
2021-05-25 18:13:41 -0500

    Create two functions inside helpers.py
    create_directory file takes path, and name as argument an create a directory
    in path it the directory does not exist yet.
    
    create_directories takes path as argument an create the diferent directories
    to classify the files in it.

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 
 def get_path():
     """ Asks to the user for the path of the directory and returns
@@ -12,7 +13,21 @@ def get_path():
     else:
         raise(ValueError)
 
+def create_directory(path, name):
+    """ Create a directory in path if the directory does not exist yet"""
+    new_path = os.path.join(path, name)
+    if not os.path.isdir(new_path):
+        subprocess.run(['mkdir', name])
+
+def create_directories(path):
+    """ Create the directories to classify the files"""
+    directories = ['images', 'pdf', 'videos', 'audio', 'spreedsheet', 'text', 'scripts', 'docs', 'other']
+    for directory in directories:
+        create_directory(path, directory)
+
+    
+
 if __name__ == "__main__":
-    get_path()
+    create_directories(get_path())
     
 
