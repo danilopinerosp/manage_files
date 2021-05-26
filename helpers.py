4ecc9f59c01ea1e373c02169b7e8fd7e398a9a82
Danilo Piñeros <dspinerosp@gmail.com>
2021-05-25 19:00:35 -0500

    Create the function rename_file and standardized_name
    rename_file takes as arguments the path, old_name an new_name and
    changes the old_name to the new_name of the file
    
    standardized_name takes path an filename as arguments an changes the name
    of the file according to the creation date and the type of the file.

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+from datetime import datetime
 
 def get_path():
     """ Asks to the user for the path of the directory and returns
@@ -25,9 +26,26 @@ def create_directories(path):
     for directory in directories:
         create_directory(path, directory)
 
+def rename_file(path, old_name, new_name):
+    """ The funcion renames a file that is in the path given as argument. Old_name is the current
+    name of the file and new_name is the name to change into"""
+    
+    old_file = os.path.join(path, old_name)
+    new_file = os.path.join(path, new_name)
+    os.rename(old_file, new_file)
+
+def standardized_name(path, filename):
+    """ Takes a path and a filename in it and return a string with the new name according to
+    the birth date of filename"""
+    path_file = os.path.join(path, filename)
+    stat = os.stat(path_file)
+    extension = path_file.split('.')[-1]
+    creation_time = datetime.fromtimestamp(stat.st_ctime).strftime('%m-%d-%Y_%H:%M:%S')
+    return '{}.{}'.format(creation_time, extension)
     
 
 if __name__ == "__main__":
-    create_directories(get_path())
+    name = standardized_name('.', 'text.txt')
+    rename_file('.', 'text.txt', name)
     
 
